6cd7b2b582881bb2d1630a77eec7699bd8f52418
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-01-22 03:01:37 +0900

    feat: 프롬프팅 2차 수정

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.LoginGithubView.as_view(), name='login_auth_github'), # 깃허브 연동
-        path('github/callback',views.LoginGithubCallbackView.as_view(),name='login_github_callback'),
+    path('github/callback',views.LoginGithubCallbackView.as_view(),name='login_github_callback'),
     path('code/view',views.CodeView.as_view(),name='code_view'),
     path('profile',views.MyPageView.as_view(),name='mypage_view'),
     path('profile/<int:project_id>',views.ProjectIDView.as_view(),name='project_id_view'),
