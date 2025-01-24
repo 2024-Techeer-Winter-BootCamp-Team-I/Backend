d4108213c852e9541150c84f1b3e454eeea8a954
LIMSEUNGJE <sjlim1217@naver.com>
2025-01-24 23:53:23 +0900

    refactor:초기세팅 수정

--- temp/dsadasng/backend/config/settings.py
+++ temp/dsadasng/backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2_4dc5wpy6u3a@ejcq89z(a*3v%1h5-@_6($fji0!hfd+^(5k!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- temp/guii/backend/config/urls.py
+++ temp/guii/backend/config/urls.py
@@ -17,7 +17,10 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+from django.urls import include
+
 urlpatterns = [
+    path('api/', include('generated_app.urls')),
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
