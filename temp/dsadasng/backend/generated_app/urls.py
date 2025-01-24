504e4f18f66c0b9daee772e207bcd5eceb5b1b14
kittae <jinkitea@naver.com>
2025-01-25 00:03:50 +0900

    temp 깃 추적 삭제 및 ignore 설정

--- temp/dsadasng/backend/generated_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('generate-image/', views.generate_image, name='generate_image'),
-    path('send-mms/', views.send_mms, name='send_mms'),
-]
