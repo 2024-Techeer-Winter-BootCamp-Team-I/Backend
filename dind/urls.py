0ae670440df52f91539243c03c62b8d063bfa6ea
kittae <jinkitea@naver.com>
2025-01-24 21:35:19 +0900

    refactor: docker in docker 비동기 처리 구현

--- dind/urls.py
+++ dind/urls.py
@@ -3,4 +3,5 @@ from .view import create_dind_handler  # or similar
 
 urlpatterns = [
     path('', create_dind_handler, name='create_dind_handler'),
+    path('task', create_dind_handler, name='create_dind_task'),
 ]
