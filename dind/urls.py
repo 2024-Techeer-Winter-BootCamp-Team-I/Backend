85ef3eaf822fedd69bb9ec6a16481113a6e4a8d4
kittae <jinkitea@naver.com>
2025-01-24 22:20:13 +0900

    fix: dind 비동기 처리 함수 수정

--- dind/urls.py
+++ dind/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
+from .tasks import create_dind_task
 from .view import create_dind_handler  # or similar
 
 urlpatterns = [
     path('', create_dind_handler, name='create_dind_handler'),
-    path('task', create_dind_handler, name='create_dind_task'),
+    path('task', create_dind_task, name='create_dind_task'),
 ]
