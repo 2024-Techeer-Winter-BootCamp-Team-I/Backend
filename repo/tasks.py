d756d9c38b8f9a53e5ae25b968a3eeeae98210a2
LIMSEUNGJE <sjlim1217@naver.com>
2025-01-31 06:46:26 +0900

    refactor:레포지토리 푸시 수정

--- repo/tasks.py
+++ repo/tasks.py
@@ -1,14 +1,14 @@
-# from celery import shared_task
-# import shutil
-# import os
-# from git import Repo, GitCommandError
-# import logging
-# from github import Github, GithubException
+from celery import shared_task
+import shutil
+import os
+from git import Repo, GitCommandError
+import logging
+from github import Github, GithubException
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 # @shared_task
-# def copy_and_push_to_github(project_dir, repo_name, username, email, access_token, organization_name=None, private=False):
+# def copy_and_push_to_github(project_dir, repo_name, username, email, access_token, private=False):
 #     """
 #     파일 복사 및 GitHub 레포지토리 생성 및 푸시를 수행하는 Celery 태스크
 #     """
@@ -20,12 +20,8 @@
 #         g = Github(access_token)
 
 #         # 레포지토리 생성
-#         if organization_name:
-#             org = g.get_organization(organization_name)
-#             repo = org.create_repo(repo_name, private=private)
-#         else:
-#             github_user = g.get_user()
-#             repo = github_user.create_repo(repo_name, private=private)
+#         github_user = g.get_user()
+#         repo = github_user.create_repo(repo_name, private=private)
 
 #         # Git 저장소 초기화
 #         local_repo = Repo.init(project_dir)
@@ -69,20 +65,8 @@
 #     except Exception as e:
 #         logger.error(f"파일 복사 및 푸시 중 오류 발생: {str(e)}", exc_info=True)
 #         raise e
-from celery import shared_task
-import shutil
-import os
-from git import Repo, GitCommandError
-import logging
-from github import Github, GithubException
-
-logger = logging.getLogger(__name__)
-
 @shared_task
 def copy_and_push_to_github(project_dir, repo_name, username, email, access_token, private=False):
-    """
-    파일 복사 및 GitHub 레포지토리 생성 및 푸시를 수행하는 Celery 태스크
-    """
     try:
         # 디렉터리 생성 (이미 존재하는 경우 무시)
         os.makedirs(project_dir, exist_ok=True)
@@ -112,8 +96,8 @@ def copy_and_push_to_github(project_dir, repo_name, username, email, access_toke
 
         # main 브랜치 생성 및 체크아웃
         if 'main' not in local_repo.heads:
-            local_repo.git.branch('main')
-        local_repo.git.checkout('main')
+            local_repo.git.branch('main')  # main 브랜치 생성
+        local_repo.git.checkout('main')  # main 브랜치로 체크아웃
 
         # 원격 저장소 설정
         remote_url = f"https://{access_token}@github.com/{username}/{repo.name}.git"
