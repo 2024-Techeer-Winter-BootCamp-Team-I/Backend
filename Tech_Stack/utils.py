fae1c4c6fe908a992e17a3855427fd8a1c12fe27
LIMSEUNGJE <sjlim1217@naver.com>
2025-01-31 16:59:46 +0900

    refactor:기술스택 동의어 추가

--- Tech_Stack/utils.py
+++ Tech_Stack/utils.py
@@ -1,5 +1,37 @@
 import os
 
+# def find_matching_template(tech_stack_name, project_type):
+#     """
+#     사용자가 입력한 기술 스택을 기반으로 적절한 템플릿을 찾습니다.
+#     """
+#     if project_type == 'frontend':
+#         templates = [
+#             "react-js-npm-vite",
+#             "react-js-npm-webpack",
+#             "react-ts-npm-vite",
+#             "react-ts-npm-webpack",
+#             "react-js-yarn-vite",
+#             "react-js-yarn-webpack",
+#             "react-ts-yarn-vite",
+#             "react-ts-yarn-webpack",
+#         ]
+#     elif project_type == 'backend':
+#         templates = [
+#             "Django_postgresql",
+#             "Django_mysql",
+#             "Django_sqlite3",
+#             "Node.js_postgresql",
+#             "Node.js_mysql",
+#         ]
+
+#     # 기술 스택을 소문자로 변환하여 비교
+#     tech_stack_name = [tech.lower() for tech in tech_stack_name]
+#     for template in templates:
+#         template_lower = template.lower()
+#         if all(tech in template_lower for tech in tech_stack_name):
+#             return os.path.join("Tech_Stack", project_type.capitalize(), template)
+
+#     return None
 def find_matching_template(tech_stack_name, project_type):
     """
     사용자가 입력한 기술 스택을 기반으로 적절한 템플릿을 찾습니다.
@@ -24,8 +56,19 @@ def find_matching_template(tech_stack_name, project_type):
             "Node.js_mysql",
         ]
 
-    # 기술 스택을 소문자로 변환하여 비교
+    # 기술 스택을 소문자로 변환
     tech_stack_name = [tech.lower() for tech in tech_stack_name]
+
+    # 동의어 사전 
+    synonym_dict = {
+        "javascript": "js",
+        "typescript": "ts",
+        "nodejs": "node.js",
+    }
+
+    # 동의어 변환
+    tech_stack_name = [synonym_dict.get(tech, tech) for tech in tech_stack_name]
+
     for template in templates:
         template_lower = template.lower()
         if all(tech in template_lower for tech in tech_stack_name):
